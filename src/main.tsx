c08fd06da665de15ad6b630a62a67c52a98c32f5
Alexander Baquero <brayanalexanderb@gmail.com>
2025-03-15 13:01:23 -0500

    Implement routing for Home component using React Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Home from "./ui/pages/Home";
 
 import "./index.css";
-import Home from "./ui/pages/Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 );
