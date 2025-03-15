b24444c352670b5a7b666140a8b13437fff84d9f
Alexander Baquero <brayanalexanderb@gmail.com>
2025-03-15 14:33:57 -0500

    Add routing for Pokemons page within MainLayout component

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,18 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Home from "./ui/pages/Home";
 
 import "./index.css";
+import MainLayout from "./ui/templates/MainLayout";
+import Pokemons from "./ui/pages/Pokemons";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/pokemon" element={<Pokemons />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
