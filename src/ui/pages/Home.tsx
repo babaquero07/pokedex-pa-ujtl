46c3d7a455058dae2322fcb966cb8ec8d3163d98
Alexander Baquero <brayanalexanderb@gmail.com>
2025-03-15 13:01:18 -0500

    Enhance Home component with layout, styling, and navigation; include header, description, and Pokedex integration

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,5 +1,31 @@
+import { Link } from "react-router";
+import Button from "../atoms/button";
+import Footer from "../molecules/Footer";
+import Pokedex from "../atoms/Pokedex";
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <main className="relative h-[75vh] bg-red-700">
+        <div className="flex flex-col items-center justify-center h-full gap-8">
+          <h1 className="text-6xl font-bold text-white">
+            Quién es ese Pokémon?
+          </h1>
+          <p className="text-[20px] text-white font-medium">
+            Explora la lista completa de Pokémon disponibles en la región y
+            descubre sus habilidades y características.
+          </p>
+
+          <Button className="text-2xl px-6">
+            <Link to="/pokemon">Explorar</Link>
+          </Button>
+        </div>
+        <Pokedex />
+      </main>
+
+      <Footer />
+    </>
+  );
 };
 
 export default Home;
